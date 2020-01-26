499796590eaee72a9b3e74dfcb1e02ba4c74c817
TheJoseph98 <josip.babok3@gmail.com>
2020-01-26 19:23:13 +0100

    adds xp gain display

--- amqCreditsGain.user.js
+++ amqCreditsGain.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         AMQ Notes Gain Display
+// @name         AMQ Notes and XP Gain Display
 // @namespace    http://tampermonkey.net/
-// @version      1.1
-// @description  Adds a display above your notes count to display how many notes you gained, disappears after 5 seconds
+// @version      1.2
+// @description  Adds 2 displays above your xp bar and note count to indicated how much you gained of each after guessing a song or at the end of the round, disappears after 5 seconds
 // @author       TheJoseph98
 // @match        https://animemusicquiz.com/*
 // @grant        none
@@ -12,23 +12,37 @@ if (!window.setupDocumentDone) return;
 
 $("#currencyText")
     .after($("<div></div>")
+        .attr("id", "expGain")
+        .css("position", "relative")
+        .css("bottom", "115px")
+        .css("left", "-175px")
+        .css("font-size", "28px")
+        .css("display", "none")
+        .text("0")
+    )
+    .after($("<div></div>")
         .attr("id", "currencyGain")
         .css("position", "relative")
         .css("bottom", "75px")
         .css("left", "20px")
-        .css("font-size", "30px")
+        .css("font-size", "28px")
         .css("display", "none")
         .text("0")
     )
 
 let $creditsGain = $("#currencyGain");
+let $expGain = $("#expGain");
 
 let creditsGainListener = new Listener("quiz xp credit gain", (data) => {
     let creditsGain = data.credit - xpBar.currentCreditCount;
+    let expGain = data.xpInfo.xpIntoLevel - Math.round(xpBar._xpPercent*10000);
     $creditsGain.text("+" + creditsGain);
     $creditsGain.show();
+    $expGain.text("+" + expGain);
+    $expGain.show();
     setTimeout(function () {
         $creditsGain.hide();
+        $expGain.hide();
     }, 5000);
 });
 
