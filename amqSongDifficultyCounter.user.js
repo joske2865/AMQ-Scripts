4956858e1621d7949f26741643dc9be56ff03880
TheJoseph98 <josip.babok3@gmail.com>
2020-06-15 08:49:12 +0200

    Version number fix

--- amqSongDifficultyCounter.user.js
+++ amqSongDifficultyCounter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AMQ Song Difficulty Counter
 // @namespace    https://github.com/TheJoseph98
-// @version      1.0
+// @version      1.1
 // @description  Counts the songs by individual difficulty, per song type
 // @author       TheJoseph98
 // @grant        GM_xmlhttpRequest
