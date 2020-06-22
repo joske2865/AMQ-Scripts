4390c2bedc11f0d3fd6b01dcd590e88114cab433
Riven Skaye <pretender0906@gmail.com>
2020-06-22 21:09:41 +0200

    Fixed several bugs in the Solo Chat Block

--- amqSoloChatBlock.user.js
+++ amqSoloChatBlock.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AMQ Solo Chat Block
 // @namespace    SkayeScripts
-// @version      0.8.1
+// @version      0.10
 // @description  Puts a nice image over the chat in solo and Ranked rooms, customizable. Improves overall performance.
 // @author       Riven Skaye // FokjeM
 // @match        https://animemusicquiz.com/*
@@ -11,7 +11,6 @@
 // @updateURL    https://github.com/FokjeM/AMQ-Scripts-1/raw/master/amqSoloChatBlock.user.js
 // ==/UserScript==
 
-// Register the script to Joseph's list of installed scripts
 const SCRIPT_INFO = {
         name: "AMQ Solo Chat Block",
         author: "RivenSkaye",
@@ -22,9 +21,7 @@ const SCRIPT_INFO = {
     };
 AMQ_addScriptData(SCRIPT_INFO);
 
-// Don't do anything on the sign-in page
 if (!window.setupDocumentDone) return;
-
 // Function to check if localStorage even exists here. If it doesn't, people are using a weird browser and we can't support them.
 function storageAvailable() {
     let storage;
@@ -38,18 +35,17 @@ function storageAvailable() {
         return false;
     }
 }
-
 /*
  * Callback function for the MutationObserver on the lobby. Should make sure the script only runs when a lobby is entered.
  */
 function lobbyOpen(mutations, observer){
     mutations.forEach((mutation) => {
-        mutation.oldValue == "text-center hidden" ? changeChat() : restoreChat();
+        mutation.oldValue == "text-center hidden" ? setTimeout(changeChat, 100) : null;
     });
 }
 // Create the observer for opening a lobby
 let lobbyObserver = new MutationObserver(lobbyOpen);
-// Start the observer
+// create and start the observer
 lobbyObserver.observe($("#lobbyPage")[0], {attributes: true, attributeOldValue: true, characterDataOldValue: true, attributeFilter: ["class"]});
 
 //These are the defaults. Laevateinn should be bliss to everyone. JQuery CSS notation since we leverage Ege's resources.
@@ -63,24 +59,30 @@ const gcC_css_default = {
     "opacity": 1.0
 };
 
-// Variables to store old and new stuff so we can unfuck later
+// Variables for setting and changing data
 let gcC_css;
 let old_gcC_css;
 let settings;
 let updateBlockLive = false;
-
-// Grab the local storage or error out
+// Fix for joining a game by invite from a modified room
+let switchGameListener = new Listener("Spectate Game", () => {
+    console.log("Joining a new game, restoring chat in 3... 2... 1...")
+    setTimeout(restoreChat, 100);
+});
+// Set settings or error out
 storageAvailable ? settings = window.localStorage : displayMessage("Browser Issue", "Your current browser or session does not support localStorage.\nGet a different browser or change applicable settings.", "Aye");
 
 /*
  * Function that actually replaces the chatbox with an image.
  * Loads in the last saved settings, or the default if nothing was set.
- * Unbinds a lot of listeners for improving performance. Mainly an issue in Ranked.
  */
 function changeChat(){
-    if(!settings || (!lobby.soloMode && !inRanked())){
+    if(!settings || (!inRanked() && lobby.settings.roomSize > 1)){
+        console.log("Restoring chat");
+        restoreChat();
         return;
     }
+    updateBlockLive = true;
     old_gcC_css = $("#gcContent").css(["backgroundImage", "backgroundRepeat", "backgroundAttachment", "backgroundPosition", "backgroundSize", "transform", "opacity"]);
     // If it's not set yet, create the object in localStorage using the defaults. Hail persistence!
     !settings.getItem("SoloChatBlock") ? localStorage.setItem("SoloChatBlock", JSON.stringify(gcC_css_default)) : null;
@@ -89,7 +91,9 @@ function changeChat(){
     // Apply the CSS and hide the chat
     $("#gcContent").css(gcC_css);
     $("#gcChatContent").css("display", "none");
+    // This is for live updates of settings
     updateBlockLive = true;
+    // unbind all listeners
     gameChat._newMessageListner.unbindListener();
 	gameChat._newSpectatorListner.unbindListener();
 	gameChat._spectatorLeftListner.unbindListener();
@@ -102,6 +106,8 @@ function changeChat(){
 	gameChat._spectatorNameChangeListner.unbindListener();
 	gameChat._deletePlayerMessagesListener.unbindListener();
 	gameChat._deleteChatMessageListener.unbindListener();
+    // If you join another game, we gotta restore the chat
+    switchGameListener.bindListener();
 }
 
 /*
@@ -122,20 +128,18 @@ function updateSettings(bg, repeat, attachment, bgpos, size, transform, opacity)
 }
 
 /*
- * The actual unfucking of the chat
- * We don't need to bind the listeners again, the game does so when entering a lobby.
- * Binding them will probably cause more issues since the game unbinds when leaving as well.
+ * Restores the chat to its original state
  */
 function restoreChat(){
+    switchGameListener.unbindListener();
     $("#gcContent").css(old_gcC_css);
-    $("#gcChatContent").css("display", "block");
+    $("#gcChatContent").css("display", "");
     updateBlockLive = false;
 }
 
 /*
- * A utility function to determine whether or not to block the chat in Ranked.
- * Defaults to NOPE
+ * Helper function to determine if the user is in Ranked
  */
 function inRanked(){
-    return settings.getItem("BlockRankedChat") ? (settings.getItem("BlockRankedChat") == "true") : function(){settings.setItem("BlockRankedChat", false); return false;}() && lobby.settings.gameMode === "Ranked";
+    return settings.getItem("BlockRankedChat") ? (settings.getItem("BlockRankedChat") == "true" && lobby.settings.gameMode === "Ranked") : function(){settings.setItem("BlockRankedChat", false); return false;}() && lobby.settings.gameMode === "Ranked";
 }
