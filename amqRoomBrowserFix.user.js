5b24518b360f54e91061164a7d42918cb27e3284
RivenSkaye <pretender0906@gmail.com>
2021-02-12 17:29:57 +0100

    Forgot the `startPage` wrapper

--- amqRoomBrowserFix.user.js
+++ amqRoomBrowserFix.user.js
@@ -9,15 +9,17 @@
 // @updateURL    https://github.com/RivenSkaye/AMQ-Scripts-1/raw/master/amqRoomBrowserFix.user.js
 // ==/UserScript==
 
-// Wait until the LOADING... screen is hidden and load script
-let loadInterval = setInterval(() => {
-    if (document.getElementById("loadingScreen").classList.contains("hidden")) {
-        // No need for setup or anything, this is the only thing it does and should only run once
-        if(ROOM_TILE_TEMPLATE){
-            ROOM_TILE_TEMPLATE = ROOM_TILE_TEMPLATE
-                                    .replace("<i class=\"fa fa-bars rbrAllOptionsIcon clickAble\" aria-hidden=\"true\" data-toggle=\"tooltip\" data-placement=\"top\" title=\"All Settings\"></i>", "")
-                                    .replace("<div class=\"rbrRoomNameContainer\">", "<i class=\"fa fa-bars rbrAllOptionsIcon clickAble\" aria-hidden=\"true\" data-toggle=\"tooltip\" style=\"margin-right: 3px;\" data-placement=\"top\" title=\"All Settings\"></i>\n\t\t\t<div class=\"rbrRoomNameContainer\">");
+if(!document.getElementById("startPage")){
+    // Wait until the LOADING... screen is hidden and load script
+    let loadInterval = setInterval(() => {
+        if (document.getElementById("loadingScreen").classList.contains("hidden")) {
+            // No need for setup or anything, this is the only thing it does and should only run once
+            if(ROOM_TILE_TEMPLATE){
+                ROOM_TILE_TEMPLATE = ROOM_TILE_TEMPLATE
+                                        .replace("<i class=\"fa fa-bars rbrAllOptionsIcon clickAble\" aria-hidden=\"true\" data-toggle=\"tooltip\" data-placement=\"top\" title=\"All Settings\"></i>", "")
+                                        .replace("<div class=\"rbrRoomNameContainer\">", "<i class=\"fa fa-bars rbrAllOptionsIcon clickAble\" aria-hidden=\"true\" data-toggle=\"tooltip\" style=\"margin-right: 3px;\" data-placement=\"top\" title=\"All Settings\"></i>\n\t\t\t<div class=\"rbrRoomNameContainer\">");
+            }
+            clearInterval(loadInterval);
         }
-        clearInterval(loadInterval);
-    }
-}, 500);
+    }, 500);
+}
