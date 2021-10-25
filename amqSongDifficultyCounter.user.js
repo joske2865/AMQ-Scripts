bbf8425cc5baedfab0563ff5984572a3fa518509
nyamu-amq <60846124+nyamu-amq@users.noreply.github.com>
2021-10-26 08:04:34 +0900

    fixed for new return to lobby

--- amqSongDifficultyCounter.user.js
+++ amqSongDifficultyCounter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AMQ Song Difficulty Counter
 // @namespace    https://github.com/TheJoseph98
-// @version      1.3
+// @version      1.3.0
 // @description  Counts the songs by individual difficulty, per song type
 // @author       TheJoseph98
 // @grant        GM_xmlhttpRequest
@@ -784,7 +784,10 @@ function setup() {
             }
         }
         // return to lobby vote
-        quiz.startReturnLobbyVote();
+        socket.sendCommand({
+            type: "quiz",
+            command: "start return lobby vote",
+        });
         saveCounterData();
     });
 
