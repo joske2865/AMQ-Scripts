5dfa3b12dba379b0750d7a5cd8679222f015577a
Graywing13 <30674558+Graywing13@users.noreply.github.com>
2022-11-30 07:54:42 -0800

    Make anime list links open in new tab
    - make MAL/Anilist/Kitsu links in the song info dialogue automatically open in new tab
    - change version number to make tampermonkey autoupdate the previous PR as well

--- amqSongListUI.user.js
+++ amqSongListUI.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AMQ Song List UI
 // @namespace    https://github.com/TheJoseph98
-// @version      3.3.3
+// @version      3.3.4
 // @description  Adds a song list window, accessible with a button below song info while in quiz, each song in the list is clickable for extra information
 // @author       TheJoseph98
 // @match        https://animemusicquiz.com/*
@@ -674,9 +674,9 @@ function updateInfo(song) {
             <a target="_blank" href="https://www.animenewsnetwork.com/encyclopedia/anime.php?id=${song.annId}">https://www.animenewsnetwork.com/encyclopedia/anime.php?id=${song.annId}</a>
         </div>`);
     let animeInfoLinksContainer = $(`<div id="animeInfoLinksContainer"><h5><b>MAL/Anilist/Kitsu IDs</b></h5><p style="margin-bottom: 0;">`
-        .concat(Number.isInteger(song.siteIds.malId) ? `</p>MAL ID: <a href="https://www.myanimelist.net/anime/${song.siteIds.malId}">${song.siteIds.malId}</a><p style="margin-bottom: 0;"` : ``)
-        .concat(Number.isInteger(song.siteIds.aniListId) ? `</p>Anilist ID: <a href="https://www.anilist.co/anime/${song.siteIds.aniListId}">${song.siteIds.aniListId}</a><p style="margin-bottom: 0;">` : ``)
-        .concat(Number.isInteger(song.siteIds.kitsuId) ? `</p>Kitsu ID: <a href="https://kitsu.io/anime/${song.siteIds.kitsuId}">${song.siteIds.kitsuId}</a><p style="margin-bottom: 0;">` : ``)
+        .concat(Number.isInteger(song.siteIds.malId) ? `</p>MAL ID: <a target="_blank" href="https://www.myanimelist.net/anime/${song.siteIds.malId}">${song.siteIds.malId}</a><p style="margin-bottom: 0;"` : ``)
+        .concat(Number.isInteger(song.siteIds.aniListId) ? `</p>Anilist ID: <a target="_blank" href="https://www.anilist.co/anime/${song.siteIds.aniListId}">${song.siteIds.aniListId}</a><p style="margin-bottom: 0;">` : ``)
+        .concat(Number.isInteger(song.siteIds.kitsuId) ? `</p>Kitsu ID: <a target="_blank" href="https://kitsu.io/anime/${song.siteIds.kitsuId}">${song.siteIds.kitsuId}</a><p style="margin-bottom: 0;">` : ``)
         .concat(`</p>`))
     let guessedContainer = $(`<div id="guessedContainer"></div>`)
         .html(`<h5><b>Guessed<br>${guesses.length}/${song.activePlayers} (${parseFloat((guesses.length/song.activePlayers*100).toFixed(2))}%)</b></h5>`);
